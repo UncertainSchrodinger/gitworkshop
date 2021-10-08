387b8eab200fdd7b7028065e3f7ff1084e90b976
Tatu Argillander <tatu.argillander@kouralabs.com>
2021-10-08 11:47:45 +0300

    Stuff for feature-a

--- code.rb
+++ code.rb
@@ -2,11 +2,13 @@ def i_will_conflict
   this = "is going to be fine"
   puts this
   feature_a
+<<<<<<< HEAD
   feature_b
+=======
+>>>>>>> b32fd92... Stuff for feature-a
 end
 
 def feature_a
-  puts "debug"
 end
 
 def feature_b

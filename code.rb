b8dbc132b1f7ea5b4fb033bc68eb6426243a4c95
Tatu Argillander <tatu.argillander@kouralabs.com>
2021-10-08 10:10:23 +0300

    This commit will conflict

--- code.rb
+++ code.rb
@@ -1,6 +1,11 @@
 def i_will_conflict
+<<<<<<< HEAD
   this = "is going to be fine"
   puts this
+=======
+  puts something
+  something = "is going to be wrong"
+>>>>>>> 59f42d0... This commit will conflict
 end
 
 i_will_conflict
